540d457f872a39e78d6f88d3fb33af5c04941af2
Marek <marek.marchlewicz@gmail.com>
2018-06-24 13:20:45 +0800

    First commit

---
+++ PlaygroundOpenGL/src/Application.cpp
@@ -0,0 +1,127 @@
+#include  <GL\glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl << std::endl;
+
+	{
+		float positions[] =
+		{
+			-0.5f, -0.5f, 0.0f, 0.0f,
+			0.5f, -0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, 1.0f, 1.0f,
+			-0.5f, 0.5f, 0.0f, 1.0f
+		};
+
+		unsigned int indices[] =
+		{
+			0, 1, 2,
+			2, 3, 0
+		};
+
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
+
+		VertexArray va;
+		VertexBuffer vb(positions, sizeof(positions));
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
+		
+		IndexBuffer ib(indices, sizeof(indices) / sizeof(unsigned int));
+
+		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1), glm::vec3(-1, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1), glm::vec3(0, 1, 1));
+
+		glm::mat4 mvp = proj * view * model;
+
+		Shader shader("res/shaders/Basic.shader");
+		shader.Bind();
+		Texture texture("res/textures/ARKit.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
+		shader.SetUniformMat4f("u_MVP", mvp);
+
+		va.Unbind();
+		vb.Unbind();
+		ib.Unbind();
+		shader.Unbind();
+		
+		Renderer renderer;
+
+		float r = 0.0f;
+		float increment = 0.05f;
+
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			renderer.Clear();
+
+			shader.Bind();
+			shader.SetUniform4f("u_Color", r, 0.2f, 1.0f, 1.0f);
+
+			renderer.Draw(va, ib, shader);
+
+			if (r > 1.0f)
+				increment = -0.05f;
+			if (r < 0.0f)
+				increment = 0.05f;
+
+			r += increment;
+
+			/* Swap front and back buffers */
+			GLCall(glfwSwapBuffers(window));
+
+			/* Poll for and process events */
+			GLCall(glfwPollEvents());
+		}
+	}
+
+	glfwTerminate();
+	return 0;
+}
